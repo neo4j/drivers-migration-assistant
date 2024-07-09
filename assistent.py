5b8966702ff1695fd1d12d658a5a2b277f2e6761
Stefano Ottolenghi <stefano.ottolenghi@neo4j.com>
2024-07-09 10:00:12 +0200

    refactor to object structure deprecated/removed counters option to remove colors

--- assistent.py
+++ assistent.py
@@ -1,11 +1,16 @@
 import click
 import json
 from sys import exit
+import re
 #import textwrap as tw
 from glob import iglob
 from tree_sitter import Language, Parser
 from packaging.version import Version
 
+
+color_deprecated = '93'  # bright red
+color_removed = '91'  # bright yellow
+color_code_highlight = '46'  # light blue
 
 welcome_warning = '''
 This is an assistant migrator for Neo4j language libraries (drivers).
@@ -34,110 +39,49 @@ To continue, type Y to confirm you've read this info or anything else to quit: '
     help='Skip the initial warning.'
 )
 @click.option(
+    '--no-output-colors', 'no_output_colors', is_flag=True, flag_value=True,
+    help="Don't enrich output with colors."
+)
+@click.option(
     '--rough-parsing', '-R', 'rough_parsing', is_flag=True, flag_value=True,
     help='Use a coarser parser. This is likely to surface more matches, but with more possible false positives as well.'
 )
-def parse(path, language_name, context_lines, version, accept_warning, rough_parsing):
-
-    language, queries = include_language(language_name)
+def parse(path, language_name, context_lines, version, accept_warning, no_output_colors, rough_parsing):
     warn_user(accept_warning)
-
+    assistent = DriverMigrationAssistent(language_name, context_lines, version, no_output_colors)
     file_paths = iglob(path.strip(), recursive=True)
+    deprecated_count = 0; removed_count = 0;
     for file_path in file_paths:
-        click.echo('\n\n\033[92;1;4mFile:\033[24m ' + file_path + '\033[0m', nl=False)
-
-        source_text = open(file_path).read()
-        source_lines = source_text.split("\n")
-
-        parser = Parser(language)
-        tree = parser.parse(bytes(source_text, 'utf8'))
-
-        messages = []
-
-        changes_json = json.loads(open(f'changelogs/{language_name}.json').read())
-        for change in changes_json:
-            captures = []
-            for pattern in change['patterns']:
-                if isinstance(pattern['pattern'], str):
-                    query = getattr(queries, pattern['type'])(pattern['pattern'])
-                elif isinstance(pattern['pattern'], list):
-                    query = getattr(queries, pattern['type'])(*pattern['pattern'])
-                else:
-                    raise ValueError('Change identifier must be str or list.')
-
-                matches = language.query(query)
-                captures += clean_captures(matches.captures(tree.root_node), pattern)
-
-            for capture in captures:
-                output = ''
-                node = capture[0]
-
-                if is_deprecated(change, version):
-                    color_code = '93'
-                elif is_removed(change, version):
-                    color_code = '91'
-                else:
-                    continue
-                output += f'\n\n\033[{color_code};1m>> ' + change['msg'].format(**change) + '\033[0m\n\n'
-                #click.echo("\n\n" + tw.indent(tw.fill("\033[91;1m" + change['msg'] + "\033[0m"), '  '))
-
-                matched_line_n = node.range.start_point[0]
-                for i in range(
-                    max(matched_line_n - context_lines, 0),
-                    min(matched_line_n + context_lines + 1, len(source_lines))
-                ):
-                    if i == matched_line_n:
-                        # highlight the matched text
-                        line_content  = source_lines[i][:node.range.start_point[1]]
-                        line_content += f'\033[46m{source_lines[i][node.range.start_point[1]:node.range.end_point[1]]}\033[0m'
-                        line_content += source_lines[i][node.range.end_point[1]:]
-                    else:
-                        line_content = source_lines[i]
-                    output += f'  \033[1m{i}\033[0m {line_content}\n'
-
-                if change.get('deprecated'):
-                    output += '\n  \033[1;4m' + 'Deprecated in:' + '\033[0m ' + change.get('deprecated')
-                if change.get('removed'):
-                    output += '\n  \033[1;4m' + 'Removed in:' + '\033[0m ' + change.get('removed')
-                if change.get('ref'):
-                    refs = change.get('ref')
-                    if isinstance(refs, str):
-                        refs = [refs, ]
-                    for link in refs:
-                        output += '\n  \033[1;4m' + 'Docs:' + '\033[0m ' + link
-                output += '\n'
-
-                messages.append({
-                    'meta': {
-                        'line': matched_line_n,
-                        'col_start': node.range.start_point[1],
-                        'col_end': node.range.end_point[1]
-                    },
-                    'content': output
-                })
-
-        messages.sort(key=lambda msg: (msg['meta']['line'], msg['meta']['col_start']))  # sort by line number; break ties by starting col number
+        messages = assistent.process_file(file_path)
         for msg in messages:
-            click.echo(msg['content'], nl=False)  # click.echo removes ANSI codes when output to file
+            assistent.print_message(msg['content'])
 
-    click.echo('\n\033[1;4m' + 'Library full manual:' + f'\033[0m https://neo4j.com/docs/{language_name}-manual/current/ \n')
+        deprecated_count += assistent.source.deprecated_count
+        removed_count += assistent.source.removed_count
+
+        assistent.print_message(
+            '\n\n\033[1;4m' + 'Deprecations in file:' +
+            f'\033[0;{color_deprecated}m {assistent.source.deprecated_count}\033[0m \n')
+        assistent.print_message(
+            '\033[1;4m' + 'Removals in file:' +
+            f'\033[0;{color_removed}m {assistent.source.removed_count}\033[0m \n')
+
+        assistent.print_message('\n' + '-'*50)
 
 
-def include_language(language_name):
-    if language_name == 'python':
-        import tree_sitter_python as tslang
-        from languagequeries.python import PythonQueries
-        queries = PythonQueries()
-    elif language_name == 'go':
-        import tree_sitter_go as tslang
-        from languagequeries.go import GoQueries
-        queries = GoQueries()
-    else:
-        raise ValueError('Invalid language. Valid choices are: python, java, go, javascript, dotnet.')
+    assistent.print_message(
+        '\n\n\033[1;4m' + 'Total deprecations:' +
+        f'\033[0;{color_deprecated}m {deprecated_count}\033[0m\n')
+    assistent.print_message(
+        '\033[1;4m' + 'Total removals:' +
+        f'\033[0;{color_removed}m {removed_count}\033[0m\n')
 
-    language = Language(tslang.language())
-
-    return language, queries
+    assistent.print_message(
+        '\n\033[1;4m' + 'Library full manual:' +
+        f'\033[0m https://neo4j.com/docs/{language_name}-manual/current/')
+    assistent.print_message(
+        '\n\033[1;4m' + 'Migration page:' +
+        f'\033[0m https://neo4j.com/docs/{language_name}-manual/current/migration/ \n\n')
 
 
 def warn_user(accept_warning):
@@ -148,23 +92,156 @@ def warn_user(accept_warning):
             exit()
 
 
-def is_deprecated(change, version):
-    return change.get('deprecated') and Version(change['deprecated']) <= Version(version)
-def is_removed(change, version):
-    return change.get('removed') and Version(change['removed']) <= Version(version)
+class DriverMigrationAssistent:
+
+    def __init__(self, language_name, context_lines, version, no_output_colors):
+        self.language_name = language_name
+        self.version = version
+        self.context_lines = context_lines
+        self.no_output_colors = no_output_colors
+        self.language, self.queries = self.include_language()
+
+    def include_language(self):
+        if self.language_name == 'python':
+            import tree_sitter_python as tslang
+            from languagequeries.python import PythonQueries
+            queries = PythonQueries()
+        elif self.language_name == 'go':
+            import tree_sitter_go as tslang
+            from languagequeries.go import GoQueries
+            queries = GoQueries()
+        else:
+            raise ValueError('Invalid language. Valid choices are: python, java, go, javascript, dotnet.')
+
+        language = Language(tslang.language())
+        return language, queries
+
+    def process_file(self, file_path):
+        self.print_message('\n\n\033[92;1;4mFile:\033[24m ' + file_path + '\033[0m')
+        self.source = File(file_path, self.language)
+        messages = []
+
+        changes_json = json.loads(open(f'changelogs/{self.language_name}.json').read())
+        for change in changes_json:
+            captures = []
+            for pattern in change['patterns']:
+                captures += self.get_captures_for_pattern(pattern)
+
+            for capture in captures:
+                msg = self.process_capture(capture, change)
+                if msg != False:
+                    messages.append(msg)
+
+        self.source.deprecated_count = self.count_deprecations(messages)
+        self.source.removed_count = self.count_removals(messages)
+
+        # sort msgs by source line number; break ties by starting col number
+        messages.sort(key=lambda msg: (msg['meta']['line'], msg['meta']['col_start']))
+        return messages
+
+    def get_captures_for_pattern(self, pattern):
+        if isinstance(pattern['pattern'], str):
+            query = getattr(self.queries, pattern['type'])(pattern['pattern'])
+        elif isinstance(pattern['pattern'], list):
+            query = getattr(self.queries, pattern['type'])(*pattern['pattern'])
+        else:
+            raise ValueError('Change identifier must be str or list.')
+
+        matches = self.language.query(query)
+        captures = self.clean_captures(matches.captures(self.source.ast.root_node), pattern)
+        return captures
+
+    def process_capture(self, capture, change):
+        output = ''
+        node = capture[0]
+
+        if self.is_deprecated(change):
+            color_code = color_deprecated
+        elif self.is_removed(change):
+            color_code = color_removed
+        else:
+            return False
+        output += f'\n\n\033[{color_code};1m>> ' + change['msg'].format(**change) + '\033[0m\n\n'
+        #click.echo("\n\n" + tw.indent(tw.fill("\033[91;1m" + change['msg'] + "\033[0m"), '  '))
+
+        matched_line_n = node.range.start_point[0]
+        for i in range(
+            max(matched_line_n - self.context_lines, 0),
+            min(matched_line_n + self.context_lines + 1, len(self.source.lines))
+        ):
+            if i == matched_line_n:
+                # highlight the matched text
+                line_content  = self.source.lines[i][:node.range.start_point[1]]
+                line_content += f'\033[{color_code_highlight}m{self.source.lines[i][node.range.start_point[1]:node.range.end_point[1]]}\033[0m'
+                line_content += self.source.lines[i][node.range.end_point[1]:]
+            else:
+                line_content = self.source.lines[i]
+            output += f'  \033[1m{i}\033[0m {line_content}\n'
+
+        if change.get('deprecated'):
+            output += '\n  \033[1;4m' + 'Deprecated in:' + '\033[0m ' + change.get('deprecated')
+        if change.get('removed'):
+            output += '\n  \033[1;4m' + 'Removed in:' + '\033[0m ' + change.get('removed')
+        if change.get('ref'):
+            refs = change.get('ref')
+            if isinstance(refs, str):
+                refs = [refs, ]
+            for link in refs:
+                output += '\n  \033[1;4m' + 'Docs:' + '\033[0m ' + link
+        output += '\n'
+
+        return {
+            'meta': {
+                'line': matched_line_n,
+                'col_start': node.range.start_point[1],
+                'col_end': node.range.end_point[1],
+                'deprecated': self.is_deprecated(change),
+                'removed': self.is_removed(change)
+            },
+            'content': output
+        }
+
+    def count_deprecations(self, messages):
+        return sum(msg['meta']['deprecated'] for msg in messages)
+
+    def count_removals(self, messages):
+        return sum(msg['meta']['removed'] for msg in messages)
+
+    def clean_captures(self, captures, pattern):
+        '''
+        Pattern change entries consisting of a list yield as many matches as
+        list elements. (Normally?) Only the last match is relevant.
+        '''
+        if isinstance(pattern['pattern'], str):
+            step_size = 1
+        elif isinstance(pattern['pattern'], list):
+            step_size = len(pattern['pattern'])
+
+        return captures[step_size-1::step_size]
+
+    def is_deprecated(self, change):
+        return (change.get('deprecated') != None and
+               Version(change['deprecated']) <= Version(self.version))
+
+    def is_removed(self, change):
+        return (change.get('removed') != None and
+               Version(change['removed']) <= Version(self.version))
+
+    def print_message(self, message):
+        if self.no_output_colors:
+            message = re.sub(r'(3|4|9|10)[0-7]', '', message)
+        # click.echo removes ANSI codes when output to file
+        click.echo(message, nl=False)
 
 
-def clean_captures(captures, pattern):
-    '''
-    Pattern change entries having a list identifier result in as many matches as list elements.
-    (Normally?) Only the last match is relevant.
-    '''
-    if isinstance(pattern['pattern'], str):
-        step_size = 1
-    elif isinstance(pattern['pattern'], list):
-        step_size = len(pattern['pattern'])
+class File:
 
-    return captures[step_size-1::step_size]
+    def __init__(self, file_path, language):
+        self.text = open(file_path).read()
+        self.lines = self.text.split('\n')
+        self.ast = Parser(language).parse(bytes(self.text, 'utf8'))
+        self.deprecated_count = 0
+        self.removed_count = 0
 
 
 if __name__ == '__main__':
